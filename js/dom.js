60c16ce0756c9498617a5dc778c641d82881ec16
tomiarg <tmoyano@claro.com.ar>
2022-09-16 10:37:41 -0300

    titulo

--- js/dom.js
+++ js/dom.js
@@ -53,7 +53,7 @@ function cargarProductos(json){
     width: 100%;
 " aria-labelledby="offcanvasExampleLabel">
     <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasExampleLabel" >Offcanvas</h5>
+      <h5 class="offcanvas-title" id="offcanvasExampleLabel" >${json[u].tela}</h5>
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body bodyCanvas">
